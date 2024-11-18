0dc06de5cee3d33456a586c7062704f7a41e56fb
yeon1216 <sykim8770@gmail.com>
2024-11-18 16:24:43 +0900

    first commit

---
+++ Parang/ParangApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParangApp.swift
+//  Parang
+//
+//  Created by BOBBY.KIM on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ParangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParangTests/ParangTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParangTests.swift
+//  ParangTests
+//
+//  Created by BOBBY.KIM on 11/18/24.
+//
+
+import Testing
+@testable import Parang
+
+struct ParangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
